f4458217760e0192aefed647903aed78e808704e
unknown <vladimirkorenevic138@gmail.com>
2021-02-22 15:07:20 +0200

    hw6

---
+++ homework-6/java.script/script.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const str = "Hello"
+
+const num = 123
+
+const flag = true
+
+const text = "true"
+
+console.log(str);
+ 
+console.log(num);
+
+console.log(flag);
+
+console.log(text);
+
+//---------2---------//
+const num1 = 228;
+
+const text1 = "hi";
+
+const name1 = null;
+
+const age1 = 12;
+
+const size = 35;
+
+let num2 = 2008;
+
+let flag1 = true;
+
+let size2 = 23;
+
+let str1 = "hi";
+
+let text2 = "false";
+
+console.log(num1);
+
+console.log(text1);
+
+console.log(name1);
+
+console.log(age1);
+
+console.log(size);
+
+console.log(num2);
+
+console.log(flag1);
+
+console.log(size2);
+
+console.log(str1);
+
+console.log(text2);
+
+
+
+
---
+++ homework-7/skript.js/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+
+//const name = prompt ( "веди імя" )//
+
+result = prompt (ведиімя)
+
+
