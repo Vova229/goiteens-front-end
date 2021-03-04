e36762266fe5135cd388316504229cbe1bebfa57
unknown <vladimirkorenevic138@gmail.com>
2021-03-04 17:25:32 +0200

    hw7

--- homework-7/skript.js/script.js
+++ homework-7/skript.js/script.js
@@ -1,8 +1,81 @@
 'use strict'
 
 
-//const name = prompt ( "веди імя" )//
+const name = prompt('ведите имя' )
 
-result = prompt (ведиімя)
+console.log(name);
+
+//-------------------------------------
+
+let friendage = alert('возраст друга 14')
+
+let myage = alert('мой возраст 12')
+
+//-------------------------------------
+
+let a = 11
+
+let b = 65
+
+console.log(a*b);
+
+//----------------------------------------
+
+let a1 = 5 + 3
+
+let a2 = 5 - 3
+
+let a3 = 5 % 3
+
+let a4 = 5 / 3
+
+console.log(a1);
+
+console.log(a2);
+
+console.log(a3);
+
+console.log(a4);
 
 
+//-----------------------------------------
+
+let a6 = alert(3 % 5);
+
+ let a7 = alert(5 % 3);
+
+ let a8 = alert(5 + '3');
+
+ let a9 = alert('5' - 3);
+
+ let a10 = alert(75 + 'кг');
+
+//---------------------------------
+
+ const width = 10
+
+ const height = 25
+
+ let S = width * height
+
+ console.log(S);
+
+//----------------------------------------
+
+const DC = 10
+
+const heightC = 4
+
+let V = DC * heightC
+
+console.log(V);
+
+//-------------------------------------
+
+let N = Math.pow(3,2)
+
+let M = Math.pow(4,2)
+
+let K = N + M
+
+console.log(K);
