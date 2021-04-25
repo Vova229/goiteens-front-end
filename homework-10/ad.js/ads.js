f121042e1a0043cff41a18148de0360f4a7857af
unknown <vladimirkorenevic138@gmail.com>
2021-04-25 16:02:07 +0300

    hw10

---
+++ homework-10/ad.js/ads.js
@@ -0,0 +1,35 @@
+'use strict'
+
+for (let t = 1; t < 11; t += 1) {
+  console.log(t);
+}
+
+console.log('-----------------------------------');
+
+for (let i = prompt('Ведите число'); i <= 10; i++) {
+  if (i % 2 == 0) {
+    alert('Число парное');
+  break;
+    }
+  else{alert('Число не парное')
+  break;
+    }
+}
+
+console.log('-----------------------------------');
+
+let num = 20;
+
+while (num < 28) {
+  console.log( num);
+  num += 1;
+}
+
+console.log('-----------------------------------');
+
+{let name = prompt('Ведите имя');}
+
+while(name.length <= 6);
+
+
+{console.log(name);}
